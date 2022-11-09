062eac3a8e6aa9be42558f3237dbe8e8ca0d6b5c
TalaSociumtech <Tala.Mohammad@sociumtech.com>
2022-11-09 19:01:42 +0300

    fix: Json file path

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string processorfilepath = ".vscode/ProcessorsFile.json";
-string taskfilepath = ".vscode/tasksFile.json";
+﻿string processorfilepath = "obj/JsonFiles/ProcessorsFile.json";
+string taskfilepath = "obj/JsonFiles/tasksFile.json";
 CPU_Scheduler.Classes.ShcedulerSJF scheduler1 = new CPU_Scheduler.Classes.ShcedulerSJF();
 scheduler1.readProcessorFile(processorfilepath);
 scheduler1.readTaskFile(taskfilepath);
