e4267a4ae41b28de5290efabdb6cd77a8a923042
TalaSociumtech <Tala.Mohammad@sociumtech.com>
2022-11-13 16:45:47 +0300

    fix: Testing

--- Classes/TestClass.cs
+++ Classes/TestClass.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
 using CPU_Scheduler.Classes;
+using CPU_Scheduler.Enums;
 public class TestClass 
 {
         List<Processor> mockProcessorList = new List<Processor>();
@@ -47,5 +48,54 @@ public class TestClass
     Assert.NotEmpty(mockCompletedTask);
    } 
 
+   [Fact]
+    public void TestCompletionTime(){
+    Processor processor = new Processor();
+    processor.pId = "p2";
+    processor.state = ProcessorState.Idle;
+    
+    CPU_Scheduler.Classes.Task task = new CPU_Scheduler.Classes.Task();
+    task.tId = "t1";
+    task.requestedTime = 5;
+    task.creationalTime = 3;
+    task.tState = TaskState.Waiting;
+    task.priority = TaskPriority.High;
 
+    mockProcessorList.Add(processor);
+    mockTaskList.Add(task);
+
+
+    ShcedulerSJF shceduler1 = new ShcedulerSJF();
+    mockCompletedTask = shceduler1.execution(mockProcessorList, mockTaskList);
+
+    int completionTime = mockCompletedTask[0].completionTime;
+    Assert.Equal(8, completionTime);
+    
+   }
+
+   [Fact]
+    public void TestWaitingTime(){
+
+      Processor processor = new Processor();
+    processor.pId = "p2";
+    processor.state = ProcessorState.Idle;
+    
+    CPU_Scheduler.Classes.Task task = new CPU_Scheduler.Classes.Task();
+    task.tId = "t1";
+    task.requestedTime = 5;
+    task.creationalTime = 3;
+    task.tState = TaskState.Waiting;
+    task.priority = TaskPriority.High;
+
+    mockProcessorList.Add(processor);
+    mockTaskList.Add(task);
+
+
+    ShcedulerSJF shceduler1 = new ShcedulerSJF();
+    mockCompletedTask = shceduler1.execution(mockProcessorList, mockTaskList);
+
+    int waitinTime = mockCompletedTask[0].waitingTime;
+    Assert.Equal(5, waitinTime);
+
+   }
 }
